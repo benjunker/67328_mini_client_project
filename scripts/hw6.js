9b89742a720d8f15b66d9c1b9432157b4a9aaa4b
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-09-28 17:46:18 -0400

    Added code to get random track

--- scripts/hw6.js
+++ scripts/hw6.js
@@ -1,2 +1,13 @@
 // CLIENT ID: 664d2bbfa8ffabfce32a4435e0d40bad
 // CLIENT SECRET: 877ea18bd6ad0f845cd7a4c6bcf44c35
+
+var url = 'https://api.soundcloud.com/tracks?client_id=664d2bbfa8ffabfce32a4435e0d40bad&genres=hiphop';
+	$.getJSON(url, function(tracks,error) {
+		if(error!='success'){
+			console.log(error);
+		} else{
+			//pick random track
+			var random_num = Math.floor(Math.random() * tracks.length);
+			console.log(tracks[random_num]);
+	}
+	});
